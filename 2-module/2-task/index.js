9acbe31122adb43fbbe967e4a4cda6a742d2790b
juliyadavydova <uzzy@list.ru>
2024-07-20 00:15:21 +0300

    Task 2.2 done

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
-function isEmpty(obj) {
-  // ваш код...
+let schedule = {};
+
+function isEmpty(obj)
+{
+    for(let key in obj)
+    {
+       if(obj[key])
+       {
+           return false
+       }
+    }
+    return true
 }
