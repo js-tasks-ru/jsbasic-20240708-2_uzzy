c8152b8d7a80e8316d3f4ced05819d465cacc739
juliyadavydova <uzzy@list.ru>
2024-08-06 22:13:00 +0300

    Task 3-4 done 2

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,34 +1,3 @@
-let user1 = {
-  "balance": "$1,825.65",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Golden Branch",
-  "gender": "male",
-  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-let user2 = {
-  "balance": "$$1,490.15",
-  "picture": "https://placehold.it/32x32",
-  "age": 25,
-  "name": "Duncan Randall",
-  "gender": "male",
-  "greeting": "Hello, Duncan Randall! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-let user3 = {
-  "balance": "$$2,790.15",
-  "picture": "https://placehold.it/32x32",
-  "age": 35,
-  "name": "Dunny Maklaud",
-  "gender": "male",
-  "greeting": "Hello, Duncan Randall! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-
 function showSalary(users, age) {
   let result = '';
   for (let i = 0; i < users.length; i++) {
