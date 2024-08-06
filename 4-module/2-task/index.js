693f3d95352a86aa59bc15207e1c99eca1c234d3
juliyadavydova <uzzy@list.ru>
2024-08-07 00:25:48 +0300

    Task 4-2 done

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,5 +1,5 @@
-let table = document.body.firstElementChild;
-for (let i = 0; i < table.rows.length; i++) {
-  let row = table.rows[i];
-  row.cells[i].style.backgroundColor = 'red';
+function makeDiagonalRed(table){
+  for (let i = 0; i < table.rows.length; i++) {
+      table.rows[i].cells[i].style.backgroundColor = 'red';
+  }
 }
