55b4943567f2c1fe24cb8a71ce4c683f64349ce3
juliyadavydova <uzzy@list.ru>
2024-08-13 17:49:40 +0300

    Task 3-4 fixed

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,14 +1,6 @@
-function showSalary(users, age) {
-  let result = '';
-  for (let i = 0; i < users.length; i++) {
-    if(users[i].age < age ){
-        if(users.indexOf(users[i]) + 2 < users.length){
-          result += `${users[i].name}, ${users[i].balance}\n`;
-        }
-      else {
-        result += `${users[i].name}, ${users[i].balance}`;
-      }
-    }
-  };
-    return result;
- };
+ function showSalary(data, age) {
+  return data
+    .filter(item => item.age <= age)
+    .map(item => `${item.name}, ${item.balance}`)
+    .join('\n');
+}
